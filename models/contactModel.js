2f5b3a2bbbbdf68e473d3628d0b7113b0ed033ed
pavanmadhur <pavanmadhurjune30@gmail.com>
2025-01-07 18:09:25 +0530

    Timing add contact

--- models/contactModel.js
+++ models/contactModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const contactSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    email: { type: String, required: true },
+    email: { type: String, required: true, index: true },
     message: { type: String, required: true },
 }, { timestamps: true });
 
